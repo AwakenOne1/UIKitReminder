698f9c148a95c8ec17693e1c66f3bad17bb72fc4
AwakenOne1 <alekseydubovik2003@icloud.com>
2024-02-01 13:23:14 +0300

    Models were created. Some work with formatted data and ListView.

---
+++ UIkitReminder/ListViewController/ReminderListViewController+DataSource.swift
@@ -0,0 +1,37 @@
+//
+//  ReminderListViewController+DataSource.swift
+//  UIkitReminder
+//
+//  Created by Alexey Dubovik on 17.01.24.
+//
+
+import UIKit
+
+extension ReminderViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+
+
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, id: String) {
+        let reminder = Reminder.sampleData[indexPath.item]
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = reminder.title
+        contentConfiguration.secondaryText = reminder.dueDate.dayAndTimeText
+        contentConfiguration.secondaryTextProperties.font = UIFont.preferredFont(
+            forTextStyle: .caption1)
+        cell.contentConfiguration = contentConfiguration
+        var backgroundConfiguration = UIBackgroundConfiguration.listGroupedCell()
+        backgroundConfiguration.backgroundColor = UIColor(red: 1.0, green: 0.8, blue: 0.9, alpha: 1.0)
+        
+    }
+    
+    private func doneButton(for reminder: Reminder) ->UICellAccessory.CustomViewConfiguration {
+        let symbolName = reminder.isComplete ? "circle.fill" : "circle."
+        let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
+        let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
+        let button = UIButton()
+        button.setImage(image, for: .normal)
+        return UICellAccessory.CustomViewConfiguration(customView: button, placement: .leading(displayed: .always))
+        
+    }
+}
