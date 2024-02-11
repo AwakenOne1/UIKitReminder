c009e1f2990305471cc411efe37d43ff7a242592
AwakenOne1 <alekseydubovik2003@icloud.com>
2024-02-11 13:58:37 +0300

    Functionality for deleting reminders added

--- UIkitReminder/ListViewController/ReminderListViewController+DataSource.swift
+++ UIkitReminder/ListViewController/ReminderListViewController+DataSource.swift
@@ -69,6 +69,11 @@ extension ReminderListViewController {
         reminders.append(reminder)
     }
     
+    func deleteReminder(with id: Reminder.ID) {
+        let index = reminders.indexOfReminder(with: id)
+        reminders.remove(at: index)
+    }
+
     private func doneButtonAccessibilityAction(for reminder: Reminder) -> UIAccessibilityCustomAction {
         let name = NSLocalizedString("Toggle completion", comment: "Accessibility label")
         let action = UIAccessibilityCustomAction(name: name) { [weak self] action in
